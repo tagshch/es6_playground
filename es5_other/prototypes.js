d91c91c9afd89014f7df65b89b43612ce4098eff
Tagir Schedrolyubov <t_shch@list.ru>
2017-02-15 12:33:06 +0300

    [es5_other] singleton add / prototypes add

--- es5_other/prototypes.js
+++ es5_other/prototypes.js
@@ -1,3 +1,5 @@
+// is prototype?
+
 var Human = function(){
   this.name = 'HUMAN';
   this.speak = function(){
@@ -8,3 +10,27 @@ var Human = function(){
 
 var John = new Human();
 John.speak();
+
+
+// prototype v2
+
+var Animal = (function(){
+
+	// --- constructor ---
+	function Animal(name){
+		this.name = name;
+	}
+
+	// --- methods ---
+	Animal.prototype.status = function(){
+		console.log(this);
+	}
+
+	return Animal;
+})();
+
+var Jepard = new Animal('Joe');
+var Cow = new Animal('Janna');
+
+Jepard.status();
+Cow.status();
