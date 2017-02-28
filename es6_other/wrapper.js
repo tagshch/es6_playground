5e9c558133e6be85205f3b936c34b5063af80d2a
Tagir Schedrolyubov <t_shch@list.ru>
2017-02-28 12:35:58 +0300

    [es6_other] wrapper for api add

--- es6_other/wrapper.js
+++ es6_other/wrapper.js
@@ -1,66 +1,109 @@
-let log = console.log;
 
-var generateTemplateString = (function () {
-    var cache = {};
+const url = require('url');
+const http = require('http');
 
-    function generateTemplate(template) {
-
-        var fn = cache[template];
-
-        if (!fn) {
-
-            // Replace ${expressions} (etc) with ${map.expressions}.
-            var sanitized = template.replace(/\$\{([\s]*[^;\s\{]+[\s]*)\}/g, function (_, match) {
-                return `\$\{map.${match.trim()}\}`;
-            });
-
-            // Afterwards, replace anything that's not ${map.expressions}' (etc) with a blank string.
-            sanitized = sanitized.replace(/(\$\{(?!map\.)[^}]+\})/g, '');
-
-            fn = Function('map', `return \`${sanitized}\``);
-
-        }
-
-        return fn;
-    }
-
-    return generateTemplate;
-})();
-
-
-
-let str = generateTemplateString('bingo ${bongo}');
-
-log(str({ bongo: 'BINGO!'}));
-log(str({}));
-
-
-
-
-/*
 let Wrapper = function (options) {
 
     let obj = {};
 
-    for (let key in options.get) {
-        obj[key] = returnRequest(options.get[key]);
-    }
-
-    function returnRequest(req) {
-        return function (obj) {
-            // dynamically generate
-
-
-            return req;
-        }
-    }
+    generateRequestList(obj, options, 'GET');
+    //generateRequestList(obj, options.root, options.post, 'POST');
 
     return obj;
 };
 
+function generateRequestList(obj, options, type){
+
+    let paths = options.get || options.post;
+
+    if(!(obj && options.root && paths && type)){
+        throw new Error('[generateRequestList] Not all arguments passed to function!');
+    }
+
+    if(type == 'GET'){
+        for(let key in paths){
+            if(!paths.hasOwnProperty(key)) continue;
+            obj[key] = generateGetMethod(options.root, paths[key], options.parseJson);
+        }
+    }
+
+    if(type == 'POST'){
+        for(let key in paths){
+            if(!paths.hasOwnProperty(key)) continue;
+            obj[key] = generatePostMethod(options.root, paths[key], options.parseJson);
+        }
+    }
+}
+
+function generateGetMethod(root, path, parseJson){
+
+    if(!(root && path)){
+        throw new Error('[generateGetMethod] No path provided!');
+    }
+
+    var templateFunction = generateTemplateFunction(path);
+
+    return function(obj, callback){
+        // here we call ajax call
+        ajaxCall(root, templateFunction(obj), parseJson, callback);
+    };
+}
+
+function ajaxCall(root, path, parseJson, callback){
+
+    let options = {
+        host: "moex.com",
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Chrome/54.0.2840.59 Safari/537.36'
+        }
+    };
+
+    options.host = root;
+    options.path = path;
+
+    let request = http.get(options, (res) => {
+        let bodyChunks = [];
+
+        res.on('data', (chunk) => {
+            bodyChunks.push(chunk);
+        });
+        res.on('end', () => {
+            let data = Buffer.concat(bodyChunks);
+
+            data = data.toString('utf-8');
+            data = parseJson ? JSON.parse(data) : data;
+
+            callback(data);
+        });
+
+    });
+
+    request.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+    });
+}
+
+
+
+// ---- helper ----
+function generateTemplateFunction(template) {
+
+    // Replace ${expressions} (etc) with ${map.expressions}.
+    var sanitized = template.replace(/\$\{([\s]*[^;\s\{]+[\s]*)\}/g, function (_, match) {
+        return `\$\{map.${match.trim()}\}`;
+    });
+
+    // Afterwards, replace anything that's not ${map.expressions}' (etc) with a blank string.
+    sanitized = sanitized.replace(/(\$\{(?!map\.)[^}]+\})/g, '');
+
+    return Function('map', `return \`${sanitized}\``);
+}
+
 
 // ---- main ----
-let engine = Wrapper({
+let engineConnector = Wrapper({
     root: 'www.moex.com',
     parseJson: true,
     get: {
@@ -69,8 +112,7 @@ let engine = Wrapper({
     }
 });
 
-let data1 = engine.list();
-let data2 = engine.single({engine: 'stock'});
+engineConnector.single({engine: 'stock'}, (res)=>{
+    console.log(res);
+});
 
-log(data1, data2);
-*/
