4a1164e028c4d11de49601eeb9e9d6dbbf6801d3
Tagir Schedrolyubov <t_shch@list.ru>
2017-03-03 17:10:28 +0300

    [es5_other] closures with anonymous function add

---
+++ es5_other/closures_2.js
@@ -0,0 +1,53 @@
+
+var log = console.log;
+
+function parseFunction(str){
+    let args = str.substring(0, str.indexOf(')'));
+    args = args.substring(str.indexOf('(') + 1).trim().split(',');
+
+    let code = str.substring(str.indexOf('{') + 1, str.lastIndexOf('}')).trim();
+
+    return new Function(args, code);
+}
+
+var obj = {
+    returnGreetingFunction: function(){
+
+        // ---- VERSION 1. NOT WORK ----
+        if(false){
+
+            var that = this;
+
+            return function(text){
+                var greet = that.GREETING;
+                console.log(greet + text);
+            };
+
+        }
+
+        // ---- VERSION 2. WORK ----
+        if(true){
+
+            self = this;
+
+            return function(text){
+                var greet = self.GREETING;
+                console.log(greet + text);
+            };
+
+        }
+
+    },
+    GREETING: 'Greeting, '
+};
+
+
+var greetingFunction = obj.returnGreetingFunction();
+
+var text = greetingFunction.toString();
+
+greetingFunction = parseFunction(text);
+
+//console.log(greetingFunction.toString());
+
+greetingFunction('BINGO!');
